2b9361b644c08941f0d9f4bdc35bd0a5a6bb3d42
David Sanders <davesque@gmail.com>
2019-02-13 00:44:56 -0700

    Basic requirements for forward mode differentiation

---
+++ ambulo/node.py
@@ -0,0 +1,193 @@
+import abc
+import math
+import operator
+from typing import (
+    Dict,
+    List,
+    NamedTuple,
+    Optional,
+    Tuple,
+    Union,
+)
+
+
+Number = Union[float, int]
+
+Name = str
+
+Env = Dict[Name, Number]
+ValEnv = Dict[Name, 'Val']
+
+
+class ReprMixin:
+    def __repr__(self):
+        return str(self)
+
+
+class OpsMixin:
+    def __add__(self, other):
+        return Add(self, other)
+
+    def __sub__(self, other):
+        return Sub(self, other)
+
+    def __mul__(self, other):
+        return Mul(self, other)
+
+    def __div__(self, other):
+        return Div(self, other)
+
+
+class VarError(Exception):
+    pass
+
+
+class Val(NamedTuple):
+    v: Optional[Number]
+    dv: Optional[Number]
+
+
+class Var(OpsMixin, ReprMixin, abc.ABC):
+    name: Name
+    outputs: List['Node']
+
+    def __init__(self, name: Name):
+        self.name = name
+        self.outputs = []
+
+    def eval(self, **env: Env) -> Number:
+        try:
+            return env[self.name]
+        except KeyError:
+            raise VarError(f'Cannot evaluate variable "{self}"')
+
+    def fwd(self, **env: ValEnv) -> Val:
+        try:
+            return env[self.name]
+        except KeyError:
+            raise VarError(f'Cannot evaluate variable "{self}"')
+
+    arity = 1
+    is_unary = True
+
+    def __str__(self):
+        return self.name
+
+
+class Node(OpsMixin, ReprMixin, abc.ABC):
+    inputs: Tuple['Node', ...]
+    outputs: List['Node']
+
+    def __init__(self, *inputs: Tuple['Node', ...]):
+        for i in inputs:
+            i.outputs.append(self)
+
+        self.inputs = inputs
+        self.outputs = []
+
+    def eval(self, **env: Env) -> Number:
+        nums = (i.eval(**env) for i in self.inputs)
+
+        return self.f(*nums)
+
+    def fwd(self, **env: ValEnv) -> Val:
+        vals = tuple(i.fwd(**env) for i in self.inputs)
+
+        vs = tuple(val.v for val in vals)
+        dvs = tuple(val.dv for val in vals)
+
+        return Val(self.f(*vs), self.df(*vs, *dvs))
+
+    @property
+    def arity(self):
+        return len(self.inputs)
+
+    @property
+    def is_unary(self):
+        return self.arity == 1
+
+    @abc.abstractmethod
+    def f(self, *args):
+        pass
+
+    @abc.abstractmethod
+    def df(self, *args):
+        pass
+
+    @abc.abstractmethod
+    def __str__(self):
+        pass
+
+
+class Unary(Node):
+    def f(self, x):
+        return self.op(x)
+
+    def __str__(self):
+        x = self.inputs[0]
+
+        return f'{self.op_str}({x})'
+
+
+class Ln(Unary):
+    op = staticmethod(math.log)
+    op_str = 'ln'
+
+    def df(self, x, dx):
+        return dx / x
+
+
+class Sin(Unary):
+    op = staticmethod(math.sin)
+    op_str = 'sin'
+
+    def df(self, x, dx):
+        return math.cos(x) * dx
+
+
+class Binary(Node):
+    op = None
+    op_str = None
+
+    def f(self, x, y):
+        return self.op(x, y)
+
+    def __str__(self):
+        x, y = self.inputs[:2]
+
+        x = f'{x}' if x.is_unary else f'({x})'
+        y = f'{y}' if y.is_unary else f'({y})'
+
+        return f'{x}{self.op_str}{y}'
+
+
+class Add(Binary):
+    op = staticmethod(operator.add)
+    op_str = ' + '
+
+    def df(self, x, y, dx, dy):
+        return dx + dy
+
+
+class Sub(Binary):
+    op = staticmethod(operator.sub)
+    op_str = ' - '
+
+    def df(self, x, y, dx, dy):
+        return dx - dy
+
+
+class Mul(Binary):
+    op = staticmethod(operator.mul)
+    op_str = ' * '
+
+    def df(self, x, y, dx, dy):
+        return x * dy + y * dx
+
+
+class Div(Binary):
+    op = staticmethod(operator.truediv)
+    op_str = ' / '
+
+    def df(self, x, y, dx, dy):
+        return (y * dx - x * dy) / (y * y)
---
+++ tests/test_node.py
@@ -0,0 +1,97 @@
+import math
+
+import pytest
+
+from ambulo.node import (
+    Node,
+    Var,
+    VarError,
+)
+
+
+class MySin(Node):
+    def f(self, x):
+        return math.sin(x)
+
+    def df(self, x, dx):
+        return dx * math.cos(x)
+
+    def __str__(self):
+        return f'sin({self.inputs[0]})'
+
+
+class MyMul(Node):
+    def f(self, x, y):
+        return x * y
+
+    def df(self, x, y, dx, dy):
+        return x * dy + y * dx
+
+    def __str__(self):
+        return f'{self.inputs[0]} * {self.inputs[1]}'
+
+
+@pytest.fixture
+def x():
+    return Var('x')
+
+
+@pytest.fixture
+def y():
+    return Var('y')
+
+
+@pytest.fixture
+def sin_x(x):
+    return MySin(x)
+
+
+@pytest.fixture
+def mul_xy(x, y):
+    return MyMul(x, y)
+
+
+def test_var_str(x):
+    assert str(x) == 'x'
+
+
+def test_var_eval(x):
+    assert x.eval(x=2) == 2
+
+
+def test_var_eval_raises_val_error(x):
+    with pytest.raises(VarError):
+        x.eval()
+
+    with pytest.raises(VarError):
+        x.eval(y=2)
+
+
+def test_var_arity(x):
+    assert x.arity == 1
+
+
+def test_var_is_unary(x):
+    assert x.is_unary
+
+
+def test_node_init_connets_nodes_both_ways(mul_xy, x, y):
+    assert x in mul_xy.inputs
+    assert y in mul_xy.inputs
+
+    assert mul_xy in x.outputs
+    assert mul_xy in y.outputs
+
+
+def test_node_eval_gets_numerical_result(mul_xy):
+    assert mul_xy.eval(x=2, y=5) == 10
+
+
+def test_node_arity(mul_xy, sin_x):
+    assert mul_xy.arity == 2
+    assert sin_x.arity == 1
+
+
+def test_node_is_unary(mul_xy, sin_x):
+    assert not mul_xy.is_unary
+    assert sin_x.is_unary
