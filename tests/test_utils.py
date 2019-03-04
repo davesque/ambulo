3cc17829f9b3f5782f298934720b106f07a10a53
David Sanders <davesque@gmail.com>
2019-03-04 14:10:52 -0700

    Add str_to_lst and tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,6 +10,7 @@ from ambulo.utils import (
     get_seq_dims,
     product,
     seq_has_dims,
+    str_to_lst,
     to_tuple,
     unflatten,
 )
@@ -227,3 +228,25 @@ def test_dot_raises_value_error(x, y):
 )
 def test_get_idx_multipliers(dims, expected):
     assert get_idx_multipliers(dims) == expected
+
+
+@pytest.mark.parametrize(
+    's, expected',
+    (
+        ('1', [[1]]),
+        ('1 2', [[1, 2]]),
+        ('1.', [[1.]]),
+        ('1 2.', [[1., 2.]]),
+        ('''
+            1 1 1
+            2 2 2
+            3 3 3
+        ''', [
+            [1, 1, 1],
+            [2, 2, 2],
+            [3, 3, 3],
+        ]),
+    ),
+)
+def test_str_to_lst(s, expected):
+    assert str_to_lst(s) == expected
